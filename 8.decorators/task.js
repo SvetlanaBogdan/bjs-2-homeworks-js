08fb9e7f202fe64da851fde2f109bb8e91ac1432
Svetlana Bogdan <123helga17@gmail.com>
2026-06-13 21:40:43 +0300

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,66 @@
-//Задача № 1
+// Задача № 1
 function cachingDecoratorNew(func) {
-  
+	const cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(args);
+		const objectInCache = cache.find((item) => item.hash === hash);
+
+		if (objectInCache) {
+			console.log('Из кеша: ' + objectInCache.value);
+			return 'Из кеша: ' + objectInCache.value;
+		}
+
+		const result = func(...args);
+
+		cache.push({
+			hash,
+			value: result,
+		});
+
+		if (cache.length > 5) {
+			cache.shift();
+		}
+
+		console.log('Вычисляем: ' + result);
+		return 'Вычисляем: ' + result;
+	}
+
+	return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId = null;
+
+	function wrapper(...args) {
+		wrapper.allCount++;
+
+		if (!timeoutId) {
+			func(...args);
+			wrapper.count++;
+
+			timeoutId = setTimeout(() => {
+				timeoutId = null;
+			}, delay);
+
+			return;
+		}
+
+		clearTimeout(timeoutId);
+
+		timeoutId = setTimeout(() => {
+			func(...args);
+			wrapper.count++;
+
+			timeoutId = setTimeout(() => {
+				timeoutId = null;
+			}, delay);
+		}, delay);
+	}
+
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	return wrapper;
 }
